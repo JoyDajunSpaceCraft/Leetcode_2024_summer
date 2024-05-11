671b84bf467d137d2e72443f9cc65f5545377aa8
JoyDajunSpaceCraft <yueluji@gmail.com>
2024-05-11 10:24:33 -0400

    857

---
+++ *857_Minimum_Cost_to_Hire_K_Workers.py
@@ -0,0 +1,21 @@
+# https://leetcode.com/problems/minimum-cost-to-hire-k-workers/?envType=daily-question&envId=2024-05-11
+class Solution:
+    def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
+        #  By default, sorted sorts tuples first by the first element, and if the first elements are the same, then by the second element.
+        ratio = sorted([(w/q, q) for w, q in zip(wage, quality)])
+        max_heap = []
+        quality_sum = 0
+        max_ratio = 0.0
+
+        for i in range(k):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1]
+            heapq.heappush(max_heap, -ratio[i][1])
+        res = max_ratio* quality_sum
+
+        for i in range(k, len(quality)):
+            max_ratio = max(max_ratio, ratio[i][0])
+            quality_sum += ratio[i][1] + heapq.heappop(max_heap)
+            heapq.heappush(max_heap, -ratio[i][1])
+            res = min(res, max_ratio* quality_sum)
+        return res
