dc146ea1ee88771024af659cab596f31cb65b07d
JoyDajunSpaceCraft <yueluji@gmail.com>
2024-05-20 10:37:56 -0400

    new

--- 131_Palindrome_Partitioning.py
+++ 131_Palindrome_Partitioning.py
@@ -1,3 +1,4 @@
+# https://programmercarl.com/0131.%E5%88%86%E5%89%B2%E5%9B%9E%E6%96%87%E4%B8%B2.html#%E5%85%B6%E4%BB%96%E8%AF%AD%E8%A8%80%E7%89%88%E6%9C%AC
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         result = []
