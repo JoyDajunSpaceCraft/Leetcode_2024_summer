2ab94481788229bcb5a49123e70bf1501c8b265c
JoyDajunSpaceCraft <yueluji@gmail.com>
2024-05-29 08:31:17 -0400

    53

---
+++ *53. Maximum Subarray.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        result = -inf
+        count = 0
+        for i in range(len(nums)):
+            count += nums[i]
+            if count > result:
+                result = count
+            if count<0:
+                count = 0
+        return result 
+        
